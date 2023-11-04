9c7e091b172d3ff5cf624c252fe5f4a947218ec1
indrianti10 <indriantiii10@gmail.com>
2023-11-05 01:13:38 +0700

    video ke-5

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,90 +31,7 @@ Route::get('/about', function () {
     ]);
 });
 
-
-
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul post kedua",
-            "author" => "Widya Dwi Indrianti",
-            "Body" =>"Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem est, quas quisquam provident molestiae blanditiis, 
-            voluptas doloremque pariatur consequuntur vel tempora maiores cum 
-            voluptatem sequi unde amet quia tenetur possimus doloribus,
-            sunt sapiente et deleniti. Dolore natus debitis voluptate? Voluptates,
-            iste vitae? Aperiam, tenetur. Asperiores, accusamus ab repellendus sint minus, 
-            necessitatibus placeat expedita soluta ut voluptatem enim cupiditate voluptate 
-            repudiandae sit laudantium quasi eaque voluptatum laboriosam, eius facere consequuntur ipsam modi eligendi labore. 
-            Voluptates amet at commodi veniam deserunt eius!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul post kedua",
-            "author" => "Ran Takahashi",
-            "Body" =>"Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem est, quas quisquam provident molestiae blanditiis, 
-            voluptas doloremque pariatur consequuntur vel tempora maiores cum 
-            voluptatem sequi unde amet quia tenetur possimus doloribus,
-            sunt sapiente et deleniti. Dolore natus debitis voluptate? Voluptates,
-            iste vitae? Aperiam, tenetur. Asperiores, accusamus ab repellendus sint minus, 
-            necessitatibus placeat expedita soluta ut voluptatem enim cupiditate voluptate 
-            repudiandae sit laudantium quasi eaque voluptatum laboriosam, eius facere consequuntur ipsam modi eligendi labore. 
-            Voluptates amet at commodi veniam deserunt eius!"
-        ]
-    ];
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-//halaman sigle post
-Route::get('posts/{slug}', function($slug){
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul post kedua",
-            "author" => "Widya Dwi Indrianti",
-            "Body" =>"Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem est, quas quisquam provident molestiae blanditiis, 
-            voluptas doloremque pariatur consequuntur vel tempora maiores cum 
-            voluptatem sequi unde amet quia tenetur possimus doloribus,
-            sunt sapiente et deleniti. Dolore natus debitis voluptate? Voluptates,
-            iste vitae? Aperiam, tenetur. Asperiores, accusamus ab repellendus sint minus, 
-            necessitatibus placeat expedita soluta ut voluptatem enim cupiditate voluptate 
-            repudiandae sit laudantium quasi eaque voluptatum laboriosam, eius facere consequuntur ipsam modi eligendi labore. 
-            Voluptates amet at commodi veniam deserunt eius!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul post kedua",
-            "author" => "Ran Takahashi",
-            "Body" =>"Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem est, quas quisquam provident molestiae blanditiis, 
-            voluptas doloremque pariatur consequuntur vel tempora maiores cum 
-            voluptatem sequi unde amet quia tenetur possimus doloribus,
-            sunt sapiente et deleniti. Dolore natus debitis voluptate? Voluptates,
-            iste vitae? Aperiam, tenetur. Asperiores, accusamus ab repellendus sint minus, 
-            necessitatibus placeat expedita soluta ut voluptatem enim cupiditate voluptate 
-            repudiandae sit laudantium quasi eaque voluptatum laboriosam, eius facere consequuntur ipsam modi eligendi labore. 
-            Voluptates amet at commodi veniam deserunt eius!"
-        ]
-    ];
-
-    $new_post = [];
-
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
   
 
