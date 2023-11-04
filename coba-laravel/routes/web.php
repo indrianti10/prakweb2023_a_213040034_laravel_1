53f9f55e56a3d457d70074decf7193b53c9dfccb
indrianti10 <indriantiii10@gmail.com>
2023-11-04 23:35:19 +0700

    video ke-3

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Widya Dwi Indrianti",
+        "email" => "widyaindrianti06@gmail.com",
+        "image" => "foto1.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
