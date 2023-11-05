94c0d366da058d68605bb26de1a098b3c0ab2f52
indrianti10 <indriantiii10@gmail.com>
2023-11-05 21:34:09 +0700

    video ke-11

--- coba-laravel/resources/views/posts.blade.php
+++ coba-laravel/resources/views/posts.blade.php
@@ -2,13 +2,14 @@
 
 @section('container')
 
-<h1 class="mb-5">Halmana Blog Posts</h1>
+<h1 class="mb-5">{{ $title }}</h1>
     
     @foreach ($posts as $post)
     <article class="mb-5 norder-bottom pb-4">
     <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2> 
     
-    <p>By. <a href="/authors/{{ $post->author->username}}" class="text-decoration-none">{{ $post->author->name }}</a> <a href="/categories/{{ $post->category->slug}}" class="text-decoration-none"> 
+    <p>By. <a href="/authors/{{ $post->author->username}}" class="text-decoration-none">{{ $post->author->name }}</a> 
+    <a href="/categories/{{ $post->category->slug}}" class="text-decoration-none"> 
     {{ $post->category->name }}</a></p>
 
     <p>{{ $post->excerpt}}</p>
