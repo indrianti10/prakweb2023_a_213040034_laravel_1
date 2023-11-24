399480f1f87bfa438cbfab19b940011f21d431bc
indrianti10 <indriantiii10@gmail.com>
2023-11-24 23:29:08 +0700

    video ke-21(Upload Image)

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -23,11 +23,12 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        // User::create([
-        //     'name' => 'Widya Dwi Indrianti',
-        //     'email' => 'widyaindrianti06@gmial.com',
-        //     'password' => bcrypt('12345')
-        // ]);
+        User::create([
+            'name' => 'Widya Dwi Indrianti',
+            'username' => 'Indrianti',
+            'email' => 'widyaindrianti06@gmial.com',
+            'password' => bcrypt('12345')
+        ]);
 
         // User::create([
         //     'name' => 'Ran Takahashi',
